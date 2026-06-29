3f4659dcc2ed395a634bba54e5210bbb62f7cd70
Nitesh0986 <nitesh99840@gmail.com>
2026-06-29 16:06:56 +0530

    Fix NLTK download for Railway

--- utils/preprocess.py
+++ utils/preprocess.py
@@ -1,6 +1,25 @@
 import re
 import nltk
 
+# -----------------------------
+# Download NLTK data if missing
+# -----------------------------
+
+def download_nltk_data():
+    resources = {
+        "corpora/stopwords": "stopwords",
+        "corpora/wordnet": "wordnet",
+        "corpora/omw-1.4": "omw-1.4",
+    }
+
+    for resource_path, resource_name in resources.items():
+        try:
+            nltk.data.find(resource_path)
+        except LookupError:
+            nltk.download(resource_name)
+
+download_nltk_data()
+
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 
@@ -9,6 +28,10 @@ lemmatizer = WordNetLemmatizer()
 
 
 def preprocess_text(text):
+    """
+    Clean resume text for NLP processing.
+    """
+
     text = text.lower()
 
     text = re.sub(r"[^a-zA-Z\s]", " ", text)
